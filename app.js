5453806d2efb90ba9142720a763b88550b8f9e47
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-08 20:44:57 +0100

    Add functionality to optionally modify the image

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const inquirer = require('inquirer');
 const { prepareOutputFileName } = require('./utils');
 
 const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
+  let image = await Jimp.read(inputFile);
+  image = await modifyImage(image);
+
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
   const textData = {
     text,
@@ -22,7 +24,8 @@ const addImageWatermarkToImage = async function (
   outputFile,
   watermarkFile
 ) {
-  const image = await Jimp.read(inputFile);
+  let image = await Jimp.read(inputFile);
+  image = await modifyImage(image);
   const watermark = await Jimp.read(watermarkFile);
   const x = image.getWidth() / 2 - watermark.getWidth() / 2;
   const y = image.getHeight() / 2 - watermark.getHeight() / 2;
@@ -33,6 +36,59 @@ const addImageWatermarkToImage = async function (
   await image.quality(100).writeAsync(outputFile);
   process.stdout.write('Success!');
   startApp();
+};
+
+const modifyImage = async (image) => {
+  const confirmModifyImage = await inquirer.prompt([
+    {
+      name: 'willModifyImage',
+      message:
+        'Would you like to make modifications to the image before adding watermark? (y /n)',
+      type: 'confirm',
+    },
+  ]);
+
+  if (confirmModifyImage.willModifyImage) {
+    //check what changes the user would like to make to the original image
+    const modificationOptions = await inquirer.prompt([
+      {
+        name: 'modificationType',
+        type: 'list',
+        message:
+          'What kind of modification would you like to add to the image?',
+        choices: [
+          'Make image brighter',
+          'Increase contrast',
+          'Make image b/w',
+          'Invert image',
+        ],
+      },
+    ]);
+    if (modificationOptions.modificationType === 'Make image brighter') {
+      const brightnessInput = await inquirer.prompt([
+        {
+          name: 'brightnessValue',
+          message: 'How would you like to change brightness? [-1, +1]',
+          default: 0.5,
+        },
+      ]);
+      return image.brightness(parseFloat(brightnessInput.brightnessValue));
+    } else if (modificationOptions.modificationType === 'Make image b/w') {
+      return image.greyscale();
+    } else if (modificationOptions.modificationType === 'Invert image') {
+      return image.invert();
+    } else if (modificationOptions.modificationType === 'Increase contrast') {
+      const contrastInput = await inquirer.prompt([
+        {
+          name: 'contrastValue',
+          message: 'How would you like to change contrast? [-1, +1]',
+          default: 0.5,
+        },
+      ]);
+      return image.contrast(parseFloat(contrastInput.contrastValue));
+    }
+  }
+  return image;
 };
 
 const startApp = async () => {
